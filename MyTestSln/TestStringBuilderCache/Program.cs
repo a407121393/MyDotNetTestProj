81e1fa91ab99077522e443c2e925f7b02a20754b
a407121393 <a407121393@163.com>
2015-02-08 22:54:40 +0800

    fix test stringbuildercache

---
+++ MyTestSln/TestStringBuilderCache/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Util;
+
+namespace TestStringBuilderCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iteration = 100000;
+
+            CodeTimer.Time("StringBuilder Spend:", iteration, () =>
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.ToString();
+            });
+            
+            CodeTimer.Time("StringBuilderCache Spend:", iteration, () =>
+            {
+                StringBuilder sb = StringBuilderCache.Acquire();
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                sb.Append("Hello World");
+                StringBuilderCache.GetStringAndRelease(sb);
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
