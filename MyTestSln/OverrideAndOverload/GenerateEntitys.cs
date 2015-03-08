6676450b52bfd04653089c45729cbb1fd0e07a80
a407121393 <a407121393@163.com>
2015-03-08 20:38:16 +0800

    t4 db

---
+++ MyTestSln/OverrideAndOverload/GenerateEntitys.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace MyProject.Entities
+
+{     
+
+        
+
+           public class product                   
+
+           {          
+                  public Int32 productid { get; set; }                                           
+
+                    
+                  public String productname { get; set; }                                           
+
+                       
+
+           }                  
+
+             
+
+           public class Person                   
+
+           {          
+                  public Int32 ID { get; set; }                                           
+
+                    
+                  public String NAME { get; set; }                                           
+
+                    
+                  public Int32 Age { get; set; }                                           
+
+                    
+                  public String Email { get; set; }                                           
+
+                    
+                  public Int32 isMan { get; set; }                                           
+
+                       
+
+           }                  
+
+             
+
+           public class test1                   
+
+           {          
+                  public Int32 ID { get; set; }                                           
+
+                    
+                  public String Name { get; set; }                                           
+
+                    
+                  public String Email { get; set; }                                           
+
+                       
+
+           }                  
+
+             
+
+           public class test2                   
+
+           {          
+                  public Int32 ID { get; set; }                                           
+
+                    
+                  public String Name { get; set; }                                           
+
+                    
+                  public String Email { get; set; }                                           
+
+                       
+
+           }                  
+
+             
+
+           public class old                   
+
+           {          
+                  public Int32 id { get; set; }                                           
+
+                    
+                  public String name { get; set; }                                           
+
+                       
+
+           }                  
+
+                           
+
+}
---
+++ MyTestSln/OverrideAndOverload/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideAndOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a=new A();
+            a.Test();
+
+            B b=new B();
+            b.Test();
+            a = b;
+            a.Test();
+
+            Console.ReadKey();
+        }
+    }
+
+    class A
+    {
+        public virtual void Test()
+        {
+            Console.WriteLine("A Base CW.");
+        }
+    }
+
+    class B:A
+    {
+        public new void Test()
+        {
+            Console.WriteLine("B Base CW.");
+        }
+    }
+}
