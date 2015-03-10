761328b3b2b78ec0a2dab9e36a802ba04d9289ca
a407121393 <a407121393@163.com>
2015-03-10 22:12:38 +0800

    decimal round 四舍五入

--- MyTestSln/TestString/Program.cs
+++ MyTestSln/TestString/Program.cs
@@ -104,25 +104,99 @@ namespace TestString
         //    Console.ReadKey();
         //}
 
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 0; i < 10000000; i++)
+        //    {
+        //        createStr(i);
+        //    }
+
+        //    GC.Collect();
+        //    GC.WaitForFullGCComplete();
+
+        //    Console.WriteLine("Done................");
+
+        //    Console.ReadKey();
+        //}
+
+        //private static void createStr(int i)
+        //{
+        //    string str = string.Intern(i.ToString());
+        //    //string str = i.ToString();
+        //}
+
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10000000; i++)
-            {
-                createStr(i);
-            }
+            //四舍六入五取偶 
+            Console.WriteLine(decimal.Round(3.5m));
+            //四舍六入五取偶 
+            Console.WriteLine(decimal.Round(4.5m));
+            //四舍六入五取偶 
+            Console.WriteLine(decimal.Round(4.6m));
+            //四舍六入五取偶 
+            Console.WriteLine(decimal.Round(3.6m));
+            //四舍六入五取偶 
+            Console.WriteLine(decimal.Round(4.7m));
+            Console.WriteLine();
 
-            GC.Collect();
-            GC.WaitForFullGCComplete();
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(3.5m));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(4.5m));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(4.6m));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(3.6m));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(4.7m));
+            Console.WriteLine();
 
-            Console.WriteLine("Done................");
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(3.5));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(4.5));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(4.6));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(3.6));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(4.7));
+            Console.WriteLine();
 
-            Console.ReadKey();
-        }
+            //四舍六入五取偶
+            Console.WriteLine(decimal.Round(0.125m, 2));
+            //四舍六入五取偶 
+            Console.WriteLine(decimal.Round(0.135m, 2));
+            //四舍六入五取偶 
+            Console.WriteLine(decimal.Round(0.126m, 2));
+            //四舍五入
+            Console.WriteLine(decimal.Round(-3.105m, 2, MidpointRounding.AwayFromZero));
+            //四舍五入
+            Console.WriteLine(decimal.Round(-0.5m, 0, MidpointRounding.AwayFromZero));
+            Console.WriteLine();
 
-        private static void createStr(int i)
-        {
-            string str = string.Intern(i.ToString());
-            //string str = i.ToString();
-        }
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(0.125m, 2));
+            //四舍六入五取偶
+            Console.WriteLine(Math.Round(0.135m, 2));
+            //四舍六入五取偶
+            Console.WriteLine(Math.Round(0.126m, 2));
+            //四舍五入
+            Console.WriteLine(Math.Round(-3.105m, 2, MidpointRounding.AwayFromZero));
+            //四舍五入
+            Console.WriteLine(Math.Round(-0.5m, 0, MidpointRounding.AwayFromZero));
+            Console.WriteLine();
+            //四舍六入五取偶  
+            Console.WriteLine(Math.Round(0.125, 2));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(0.135, 2));
+            //四舍六入五取偶 
+            Console.WriteLine(Math.Round(0.126, 2));
+            //四舍五入
+            Console.WriteLine(Math.Round(-3.105, 2, MidpointRounding.AwayFromZero));
+            //四舍五入
+            Console.WriteLine(Math.Round(-0.5, 0, MidpointRounding.AwayFromZero));
+            Console.WriteLine();
+        } 
     }
 }
